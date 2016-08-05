aa05be3228927dcf0bac73f34bf9f71e06247216
Robert Prine <robertprine2@yahoo.com>
2016-08-05 12:42:05 -0400

    Added Post model and modified current views and css for said views

---
+++ FantaC/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FantaC.Models
+{
+    public class Comment
+    {
+    }
+}
---
+++ FantaC/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FantaC.Models
+{
+    public class Post
+    {
+        [Key]
+        public string PostId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PostName { get; set; }
+
+        [Required]
+        public string PostSubject { get; set; }
+
+        public string PostImage { get; set; }        
+
+        [Required]
+        public string PostContent { get; set; }
+
+        public virtual List<Comment> PostComments { get; set; }
+    }
+}
