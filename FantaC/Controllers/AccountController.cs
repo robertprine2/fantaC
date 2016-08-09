3e3a06c2560c2ccb3502b7eb2cf948f8bc49f5f4
Robert Prine <robertprine2@yahoo.com>
2016-08-09 16:18:53 -0400

    fixed a bug where you couldn't log in outside of just registering. Started fixing where buttons would redirect you.

--- FantaC/Controllers/AccountController.cs
+++ FantaC/Controllers/AccountController.cs
@@ -15,6 +15,7 @@ namespace FantaC.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
@@ -75,7 +76,11 @@ namespace FantaC.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+
+            var user = db.Users.Where(u => u.Email.Equals(model.Email)).Single(); // where db is ApplicationDbContext instance
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
+
+            // var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
